4f63cdf7800fa9d70bdd387f6eee66ec28c35141
olucheye <seyefree2rhyme@yahoo.com>
2020-05-13 20:32:49 +0100

    update route defined with validation

--- index.js
+++ index.js
@@ -110,6 +110,7 @@ app.delete('/api/users/:id', (req,res)=>{
     User.findOneAndDelete({_id:id})
         .then(user => {
             if(!user){
+                //if it cannot find the specific user ID, it returns this error
                 res.status(404).json({
                     success: false,
                     message: "The user with the specified ID does not exist."
@@ -124,6 +125,41 @@ app.delete('/api/users/:id', (req,res)=>{
         .catch(err => res.status(500).json({
             success: false,
             errorMessage: "The user could not be removed"
+        }));
+});
+
+//@ ROUTE: PUT/UPDATE SPECIFIC USER
+//@ desc: Updates user information by first searching for its ID from the DB
+app.put('/api/users/:id', (req,res)=>{
+    const {id} = req.params;
+    const{name, bio} = req.body;
+
+    User.findOneAndUpdate(
+        {_id : id}, {name: name, bio: bio}, {overwrite: true}
+    )
+        .then(user=> {
+            if (!user) {
+                //if it cannot find the specific user ID, it returns this error
+                res.status(404).json({
+                    success: false,
+                    message: "The user with the specified ID does not exist."
+                })
+            }if(user && name === "" || bio === ""){
+                res.status(400).json({
+                    success: false,
+                    errorMessage: "Please provide name and bio for the user."
+                })
+            }else{
+                res.status(200).json({
+                    success: true,
+                    messasge: `${user.name} was successfully updated`,
+                    data: user
+                })
+            }
+        })
+        .catch(err => res.status(500).json({
+            success: false,
+            errorMessage: "The user information could not be modified."
         }));
 });
 
